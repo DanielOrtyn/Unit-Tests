2743db84df165a0e71986f74f4483bf13ebcd47f
Daniel Ortyn <DanielOrtyn@comcast.net>
2019-04-02 15:56:27 -0400

    01, Fibonacci sequence finished

--- problems/01-fibonacci/01-fibonacci.js
+++ problems/01-fibonacci/01-fibonacci.js
@@ -2,5 +2,23 @@
 Define function: fib(n) 
 Return the nth number in the fibonacci sequence. */
 function fib(n) {
+    if(n===0)
+        return 0
+    if(n===1)
+        return 1
+    return fibAdder(n-1,0,1)
+}
 
+function fibAdder(n, firstNumber, secondNumber) {
+    if(n===0) {
+        return secondNumber
+    }
+    return fibAdder(n-1, secondNumber, firstNumber + secondNumber)
+}
+
+
+
+// basic test
+for(let i = 0; i<10; i++){
+    console.log(fib(i))
 }
