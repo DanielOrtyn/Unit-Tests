541de57de7c8b041d7c654986f06c6d00f0fc72a
Daniel Ortyn <DanielOrtyn@comcast.net>
2019-04-03 11:20:30 -0400

    Problems 05 through 08 completed

--- problems/07-palindrome/07-palindrome.js
+++ problems/07-palindrome/07-palindrome.js
@@ -2,5 +2,14 @@
 Define function isPalindrome(someStr)
 Return true if someStr is a palindrome, otherwise return false */
 function isPalindrome(someStr) {
-  
+    for(let i = 0; i<someStr.length; i++){
+        if(!(someStr[i] === someStr[someStr.length-i-1])){
+            return false
+        }
+    }
+    return true
 }
+
+// basic test
+console.log(isPalindrome(`test`))
+console.log(isPalindrome(`kayak`))
--- problems/08-shapes/08-shapes.js
+++ problems/08-shapes/08-shapes.js
@@ -21,5 +21,59 @@ Example for printShape("Diamond", 5, "*");
   * 
 */
 function printShape(shape, height, character) {
+  switch (shape) {
+    case `Square`:
+      drawSquare(height, character)
+      break
+    case `Triangle`:
+      drawTriangle(height, character)
+    break
+    case `Diamond`:
+      drawDiamond(height, character)
+    break;
   
+    default:
+      break;
+  }
 }
+
+function drawSquare(height, character){
+  for(let i = 0; i<height; i++){
+    console.log(stringMultiple(height,character))
+  }
+}
+
+function drawTriangle(height, character){
+  for(let i = 1; i<=height; i++){
+    console.log(stringMultiple(i, character))
+  }
+}
+
+function drawDiamond(height, character){
+  for(let i = 0; i<Math.floor(height/2); i++){
+    let preSpaces = Math.floor(height/2-i)
+    console.log(`${stringMultiple(preSpaces, ` `)}${stringMultiple(1+2*i, character)}`)
+  }
+  if(height%2!=0){
+    console.log(stringMultiple(height, character))
+  }
+  for(let i = Math.floor(height/2)-1; i>=0; i--){
+    let preSpaces = Math.floor(height/2-i)
+    console.log(`${stringMultiple(preSpaces, ` `)}${stringMultiple(1+2*i, character)}`)
+  }
+}
+
+function stringMultiple(number, character){
+  let multipleArray = new Array()
+  for(let i = 0; i<number; i++) {
+    multipleArray.push(character)
+  }
+  return multipleArray.join(``)
+}
+
+
+printShape("Square", 4, "%")
+console.log()
+printShape("Triangle", 4, "$")
+console.log()
+printShape("Diamond", 6, "*")
