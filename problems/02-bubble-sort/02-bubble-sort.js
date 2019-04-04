f6d127c690dcc266eab7cac64fc7ab4ca56d4d06
Daniel Ortyn <DanielOrtyn@comcast.net>
2019-04-04 11:26:17 -0400

    Improvement to bubble sort

--- problems/02-bubble-sort/02-bubble-sort.js
+++ problems/02-bubble-sort/02-bubble-sort.js
@@ -6,7 +6,7 @@ function bubbleSort(numArray) {
     if(!Array.isArray(numArray)){
         return
     }
-    for(let i = 0; i<numArray.length; i++){
+    for(let i = 0; i<numArray.length-1; i++){
         let maxElement = numArray.length-i-1;
         for(let j = 0; j<maxElement; j++){
             if(numArray[j]>numArray[j+1]){
